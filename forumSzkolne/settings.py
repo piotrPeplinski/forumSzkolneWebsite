9057e84c4e576dec9a8e1f1a4e4ef065973d254a
Piotr Pepliński <p.peplaj@gmail.com>
2020-11-02 20:53:02 +0100

    created custom user model, done first migration - ability to change authentication

--- forumSzkolne/settings.py
+++ forumSzkolne/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'forum',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.User'
